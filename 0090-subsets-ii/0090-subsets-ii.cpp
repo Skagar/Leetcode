e6acf25d0f317962b6b71a00deb2b6c9fafc4efa
Samaksha Agarwal <gsamaksha@gmail.com>
2025-08-16 12:54:53 +0530

    Time: 0 ms (100%), Space: 10.4 MB (66.8%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,7 +2,8 @@
 #include <bits/stdc++.h> 
 class Solution {
 public:
-void calsub(int ind,int n,vector<int>&nums,set<vector<int>>&ans,vector<int>&temp)
+//Brute force Approach
+/*void calsub(int ind,int n,vector<int>&nums,set<vector<int>>&ans,vector<int>&temp) 
 {
  if(ind==n)
  {
@@ -13,13 +14,37 @@ temp.push_back(nums[ind]);
 calsub(ind+1,n,nums,ans,temp);
 temp.pop_back();
 calsub(ind+1,n,nums,ans,temp);
+}*/
+
+
+
+void calsub(int ind,int n,vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp) 
+{
+     ans.push_back(temp);
+for(int i=ind;i<n;i++)
+{
+    if((i!=ind)&& (nums[i]==nums[i-1]))
+    continue;
+    temp.push_back(nums[i]);
+    calsub(i+1,n,nums,ans,temp);
+    temp.pop_back();
+}
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>ans;
+    //Brute forec approach of using set and then transferring it into vector of vector
+    /*set<vector<int>>ans; 
     vector<int>temp;
     sort(nums.begin(),nums.end());
     calsub(0,nums.size(),nums,ans,temp);
     vector<vector<int>>final(ans.begin(),ans.end());
-    return final;
+    return final;*/
+    
+
+    //Better approach
+     vector<int>temp;
+    sort(nums.begin(),nums.end());
+     vector<vector<int>>ans;
+    calsub(0,nums.size(),nums,ans,temp);
+    return ans;
     }
 };
