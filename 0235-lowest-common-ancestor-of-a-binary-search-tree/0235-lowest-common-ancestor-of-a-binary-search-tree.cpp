2938417ebc77b7bb1fc59ef67967e47fb635b512
Samaksha Agarwal <gsamaksha@gmail.com>
2025-09-30 18:57:17 +0530

    Time: 23 ms (53.25%), Space: 23.7 MB (33.52%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,18 +11,32 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        // Method 1:
+        /* if (root == NULL)
+             return NULL;
+         if (root->val == p->val || root->val == q->val) {
+             return root;
+         }
+         TreeNode* leftans = lowestCommonAncestor(root->left, p, q);
+         TreeNode* rightans = lowestCommonAncestor(root->right, p, q);
+         if (leftans != NULL && rightans == NULL)
+             return leftans;
+         if (leftans == NULL && rightans != NULL)
+             return rightans;
+         if (leftans != NULL && rightans != NULL)
+             return root;
+         return NULL;*/
+
+        // Method 2:
         if (root == NULL)
             return NULL;
-        if (root->val == p->val || root->val == q->val) {
-            return root;
-        }
-        TreeNode* leftans = lowestCommonAncestor(root->left, p, q);
-        TreeNode* rightans = lowestCommonAncestor(root->right, p, q);
-        if (leftans != NULL && rightans == NULL)
-            return leftans;
-        if (leftans == NULL && rightans != NULL)
-            return rightans;
-        if (leftans != NULL && rightans != NULL)
+
+        if (root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else if (root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        else
             return root;
         return NULL;
     }
