9440131e9565a46e6bba8ec59583b530f41ce972
Samaksha Agarwal <gsamaksha@gmail.com>
2026-02-03 21:09:05 +0530

    Time: 10 ms (65.96%), Space: 14 MB (62.55%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -29,14 +29,6 @@ public:
                     }
                     j++;
                 } else {
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k + 1]) {
-                        k--;
-                    }
                     return sum;
                 }
             }
