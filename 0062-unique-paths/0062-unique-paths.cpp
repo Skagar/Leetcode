3930237808f0b713f6f665127359e242d80e4c74
Samaksha Agarwal <gsamaksha@gmail.com>
2026-03-13 20:03:44 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    int countpaths(int r, int c, int& m, int& n, vector<vector<int>>& dp) {
+    // Memoization
+    /*int countpaths(int r, int c, int& m, int& n, vector<vector<int>>& dp) {
         if (r == m - 1 && c == n - 1)
             return 1;
         if (r < 0 || c < 0 || r >= m || c >= n)
@@ -10,16 +11,35 @@ class Solution {
         int down = countpaths(r + 1, c, m, n, dp);
         dp[r][c] = right + down;
         return dp[r][c];
-    }
+    }*/
 
 public:
     int uniquePaths(int m, int n) {
+        /* 1.Memoization
+         if (m == 1 || n == 1)
+             return 1;
+         vector<vector<int>> dp(m, vector<int>(n, -1));
+         int r = 0;
+         int c = 0;
+         countpaths(r, c, m, n, dp);
+         return dp[0][0];*/
+        vector<vector<int>> dp(m, vector<int>(n, 0));
         if (m == 1 || n == 1)
             return 1;
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int r = 0;
-        int c = 0;
-        countpaths(r, c, m, n, dp);
+        dp[m - 1][n - 1] = 1;
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (r == m - 1 && c == n - 1)
+                    continue;
+                int right = 0;
+                int down = 0;
+                if (c + 1 < n)
+                    right = dp[r][c + 1];
+                if (r + 1 < m)
+                    down = dp[r + 1][c];
+                dp[r][c] = right + down;
+            }
+        }
         return dp[0][0];
     }
 };
