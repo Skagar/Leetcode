9e9fa981144237fa520170374a031b58c975123b
Samaksha Agarwal <gsamaksha@gmail.com>
2026-03-18 00:37:05 +0530

    Time: 149 ms (17.13%), Space: 67.3 MB (12.33%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -6,30 +6,30 @@ public:
         int r = 0;
         map<int, int> fq;
         int cnt = 0;
-        while (r < n) {
+        while (r < n) {  //Subarrays that have K or less Distinct elements
             fq[nums[r]]++;
-            while (l <= r && fq.size() == k) {
-                cnt += (n - r);
+            while (l <= r && fq.size() > k) {
                 fq[nums[l]]--;
                 if (fq[nums[l]] == 0)
                     fq.erase(nums[l]);
                 l++;
             }
+            cnt += (r - l + 1);
             r++;
         }
         l = 0;
         r = 0;
         map<int, int> fq1;
         int cnt1 = 0;
-        while (r < n) {
+        while (r < n) {//Subarrays that have less than K Distinct elements
             fq1[nums[r]]++;
-            while (l <= r && fq1.size() > k) {
-                cnt1 += (n - r);
+            while (l <= r && fq1.size() >= k) {
                 fq1[nums[l]]--;
                 if (fq1[nums[l]] == 0)
                     fq1.erase(nums[l]);
                 l++;
             }
+            cnt1 += (r - l + 1);
             r++;
         }
 
