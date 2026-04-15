f008614721d8daea1628c61f9f59f7ea9adea343
Samaksha Agarwal <gsamaksha@gmail.com>
2026-04-15 17:16:46 +0530

    Time: 109 ms (65.91%), Space: 110.4 MB (38.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,9 @@
 class Solution {
 private:
-    bool check(int ind, int& n, int tg, vector<int>& nums,
+   
+   //Memoization
+
+   /* bool check(int ind, int& n, int tg, vector<int>& nums,
                vector<vector<int>>& dp) {
         if (tg == 0)
             return true;
@@ -14,15 +17,31 @@ private:
             tk = check(ind - 1, n, tg - nums[ind], nums, dp);
         return dp[ind][tg] = tk || ntk;
     }
-
+*/
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>>dp(n, vector<int>(sum + 1, -1));
         if (sum % 2 != 0)
             return false;
         int tg = sum / 2;
-        return check(n - 1, n, tg, nums, dp);
+        vector<vector<int>> dp(n, vector<int>(tg + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        if (nums[0] <= tg)
+            dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 1; t <= tg; t++) {
+                bool nt = dp[i-1][t];
+                bool tk = false;
+                if (nums[i] <= t) {
+                    tk = dp[i - 1][t - nums[i]];
+                }
+                dp[i][t] = tk | nt;
+            }
+        }
+        return dp[n - 1][tg];
     }
 };
