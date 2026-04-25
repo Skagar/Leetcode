cae3513514092ad5c03004d082385a67baa6568f
Samaksha Agarwal <gsamaksha@gmail.com>
2026-04-25 12:45:25 +0530

    Time: 39 ms (42.95%), Space: 48.9 MB (33.3%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
-    int countcoins(int ind, int tg, vector<int>& coins,
+    // MEMOIZATION
+    /*int countcoins(int ind, int tg, vector<int>& coins,
                    vector<vector<int>>& dp) {
         if (tg == 0)
             return 0;
@@ -20,16 +21,34 @@ private:
         dp[ind][tg] = min(tk, ntk);
         return dp[ind][tg];
     }
-
+*/
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         int tg = amount;
         vector<vector<int>> dp(n, vector<int>(tg + 1, -1));
-        dp[n - 1][tg] = countcoins(n - 1, tg, coins, dp);
-        long long val = dp[n - 1][tg];
-        if (val == 1e9)
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= tg; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = i / coins[0];
+            else
+                dp[0][i] = 1e9;
+        }
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 0; target <= tg; target++) {
+                int tk = INT_MAX;
+                int ntk = INT_MAX;
+                ntk = 0 + dp[ind - 1][target];
+                if (coins[ind] <= target)
+                    tk = 1 + dp[ind][target - coins[ind]];
+                dp[ind][target] = min(tk, ntk);
+            }
+        }
+        if (dp[n - 1][tg] == 1e9)
             return -1;
-        return val;
+        else
+            return dp[n - 1][tg];
     }
 };
