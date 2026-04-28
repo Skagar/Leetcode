7842f3ba9fe3c8eaab034c2459f9ccaa8fae5176
Samaksha Agarwal <gsamaksha@gmail.com>
2026-04-28 22:09:37 +0530

    Time: 78 ms (53.39%), Space: 75.8 MB (73.67%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -18,7 +18,34 @@ private:
 public:
     int longestPalindromeSubseq(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return calmaxlength(0, n - 1, s, dp);
+        /*vector<vector<int>> dp1(n, vector<int>(n, -1));
+        calmaxlength(0, n - 1, s, dp1);*/
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        for (int ind1 = 0; ind1 < n; ind1++) {
+            dp[ind1][ind1] = 1;
+        }
+        for (int ind1 = 0; ind1 < n; ind1++) {
+            for (int ind2 = 0; ind2 < n; ind2++) {
+                if (ind1 > ind2)
+                    dp[ind1][ind2] = 0;
+            }
+        }
+        for (int ind1 = n - 1; ind1 >= 0; ind1--) {
+            for (int ind2 = 0; ind2 < n; ind2++) {
+                if (ind1 < ind2) {
+                    if (s[ind1] == s[ind2]) {
+                        if (ind1 + 1 < n && ind2 - 1 >= 0)
+                            dp[ind1][ind2] = 2 + dp[ind1 + 1][ind2 - 1];
+                    } else {
+                        if (ind1 + 1 < n && ind2 - 1 >= 0)
+                            dp[ind1][ind2] =
+                                max(dp[ind1 + 1][ind2], dp[ind1][ind2 - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 };
