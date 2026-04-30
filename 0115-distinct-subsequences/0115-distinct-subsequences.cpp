3b43c78ad9b1cc94653c53cc7041fc6833adcfc7
Samaksha Agarwal <gsamaksha@gmail.com>
2026-04-30 18:31:54 +0530

    Time: 31 ms (52.14%), Space: 26.9 MB (62.64%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     int countways(int ind1, int ind2, string& s, string& t,
                   vector<vector<int>>& dp) {
-         if (ind2==0)
+        if (ind2 == 0)
             return 1;
         if (ind1 == 0)
             return 0;
@@ -10,10 +10,10 @@ private:
             return dp[ind1][ind2];
         int tk = 0;
         int ntk = 0;
-        if (s[ind1-1] == t[ind2-1]) {
+        if (s[ind1 - 1] == t[ind2 - 1]) {
             tk += countways(ind1 - 1, ind2 - 1, s, t, dp);
         }
-        ntk = countways(ind1 - 1, ind2, s, t, dp);
+        ntk += countways(ind1 - 1, ind2, s, t, dp);
         return dp[ind1][ind2] = tk + ntk;
     }
 
@@ -21,8 +21,18 @@ public:
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // for (int ind1 = 0; ind1 <= n; ind1++) {
+        //     dp[ind1][0] = 1;
+        // }
+        // for (int ind2 = 0; ind2 <= m; ind2++) {
+        //     dp[0][ind2] = 0;
+        // }
+        // for (int ind1 = 1; ind1 <= n; ind1++) {
+        //     for (int ind2 = 1; ind2 <= m; ind2++) {
 
+        //     }
+        // }
         return countways(n, m, s, t, dp);
     }
 };
