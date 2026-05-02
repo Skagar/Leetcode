58034654e5de497b3d6aab5cee3548fe3341901b
Samaksha Agarwal <gsamaksha@gmail.com>
2026-05-02 21:49:30 +0530

    Time: 6 ms (70.43%), Space: 12.9 MB (81.03%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int calminoperations(int i, int j, string& word1, string& word2,
+                         vector<vector<int>>& dp) {
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (word1[i] == word2[j])
+            return dp[i][j] =
+                       0 + calminoperations(i - 1, j - 1, word1, word2, dp);
+        return dp[i][j] =
+                   1 + min(calminoperations(i - 1, j - 1, word1, word2, dp),
+                           min(calminoperations(i, j - 1, word1, word2, dp),
+                               calminoperations(i - 1, j, word1, word2, dp)));
+    }
+
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        if (len1 == 0)
+            return len2;
+        vector<vector<int>> dp(len1, vector<int>(len2, -1));
+        return calminoperations(len1 - 1, len2 - 1, word1, word2, dp);
+    }
+};
