b4a9b88e124d6e7171c40dfa9accc29612d8348d
Samaksha Agarwal <gsamaksha@gmail.com>
2026-05-03 12:49:43 +0530

    Time: 2 ms (94.22%), Space: 13.1 MB (72.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -23,6 +23,25 @@ public:
         if (len1 == 0)
             return len2;
         vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, -1));
-        return calminoperations(len1, len2, word1, word2, dp);
+        // Memoization
+        //  return calminoperations(len1, len2, word1, word2, dp);
+
+        for (int j = 0; j <= len2; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = 0 + dp[i - 1][j - 1];
+                else {
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1],
+                                       min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 };
