778a9d0efb85f2830118bc3d073ea2dcbb7b53d5
Samaksha Agarwal <gsamaksha@gmail.com>
2026-06-03 12:43:13 +0530

    Time: 15 ms (12.05%), Space: 24.6 MB (28.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,7 +21,26 @@ private:
 
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
-        return calmaxprofit(0, 1, prices, dp);
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(2, -1));
+        /* int val = calmaxprofit(0, 1, prices, dp);
+         return val;*/
+        int n = prices.size();
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 1; buy >= 0; buy--) {
+                int buyallow = -1e9;
+                int notbuy = -1e9;
+                if (buy == 1) {
+                    buyallow =
+                        max(-prices[ind] + dp[ind + 1][0], 0 + dp[ind + 1][1]);
+                } else {
+                    notbuy =
+                        max(prices[ind] + dp[ind + 1][1], 0 + dp[ind + 1][0]);
+                }
+                dp[ind][buy] = max(buyallow, notbuy);
+            }
+        }
+        return dp[0][1];
     }
 };
