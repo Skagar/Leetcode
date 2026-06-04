6c0a1823c3d6bc7d54e57b9705b39e0a3e027fda
Samaksha Agarwal <gsamaksha@gmail.com>
2026-06-04 23:34:11 +0530

    Time: 7 ms (58.18%), Space: 18.2 MB (39.4%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -19,13 +19,44 @@ private:
             notbuyallow =
                 max(prices[ind] + calmaxprofit(ind + 1, 1, k - 1, prices, dp),
                     0 + calmaxprofit(ind + 1, 0, k, prices, dp));
-        return dp[ind][buy][k]=max(buyallow, notbuyallow);
+        return dp[ind][buy][k] = max(buyallow, notbuyallow);
     }
 
 public:
     int maxProfit(int k, vector<int>& prices) {
         vector<vector<vector<int>>> dp(
-            prices.size(), vector<vector<int>>(2, vector<int>(k + 1, -1)));
-        return calmaxprofit(0, 1, k, prices, dp);
+            prices.size() + 1, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+
+        // return calmaxprofit(0, 1, k, prices, dp);
+
+        // Tabulation
+        int n = prices.size();
+        for (int buy = 0; buy <= 1; buy++) {
+            for (int j = 0; j <= k; j++) {
+                dp[n][buy][j] = 0;
+            }
+        }
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                dp[ind][buy][0] = 0;
+            }
+        }
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int K = 1; K <= k; K++) {
+                    int buyallow = -1e9;
+                    int notbuyallow = -1e9;
+                    if (buy == 1) {
+                        buyallow = max(-prices[ind] + dp[ind + 1][0][K],
+                                       0 + dp[ind + 1][1][K]);
+                    } else {
+                        notbuyallow = max(prices[ind] + dp[ind + 1][1][K - 1],
+                                          0 + dp[ind + 1][0][K]);
+                    }
+                    dp[ind][buy][K] = max(buyallow, notbuyallow);
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
