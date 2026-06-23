85b7dd1b1480152358525a8cc23ab6866ef09820
Samaksha Agarwal <gsamaksha@gmail.com>
2026-06-23 18:31:17 +0530

    Time: 0 ms (100%), Space: 10.9 MB (16.84%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,46 +1,50 @@
 class Solution {
 private:
-    int callength(int ind, int& n, int sign, vector<int>& nums) {
+    int callength(int ind, int& n, int sign, vector<int>& nums,
+                  vector<vector<int>>& dp) {
         if (ind >= n)
             return 0;
+        if (dp[ind][sign + 1] != -1)
+            return dp[ind][sign + 1];
         int tk = 0;
         int ntk = 0;
         if (sign == 0) {
             if (ind + 1 < n) {
                 int s = nums[ind + 1] - nums[ind];
                 if (s > 0)
-                    tk = 1 + callength(ind + 1, n, 1, nums);
+                    tk = 1 + callength(ind + 1, n, 1, nums, dp);
                 else if (s < 0)
-                    tk = 1 + callength(ind + 1, n, -1, nums);
+                    tk = 1 + callength(ind + 1, n, -1, nums, dp);
                 else
-                    ntk = callength(ind + 1, n, 0, nums);
+                    ntk = callength(ind + 1, n, 0, nums, dp);
             } else
                 return 1;
         } else if (sign == 1) {
             if (ind + 1 < n) {
                 int s = nums[ind + 1] - nums[ind];
                 if (s < 0)
-                    tk = 1 + callength(ind + 1, n, -1, nums);
+                    tk = 1 + callength(ind + 1, n, -1, nums, dp);
                 else
-                    ntk = callength(ind + 1, n, sign, nums);
+                    ntk = callength(ind + 1, n, sign, nums, dp);
             } else
                 return 1;
         } else if (sign == -1) {
             if (ind + 1 < n) {
                 int s = nums[ind + 1] - nums[ind];
                 if (s > 0)
-                    tk = 1 + callength(ind + 1, n, 1, nums);
+                    tk = 1 + callength(ind + 1, n, 1, nums, dp);
                 else
-                    ntk = callength(ind + 1, n, sign, nums);
+                    ntk = callength(ind + 1, n, sign, nums, dp);
             } else
                 return 1;
         }
-        return tk + ntk;
+        return dp[ind][sign + 1] = tk + ntk;
     }
 
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        return callength(0, n, 0, nums);
+        vector<vector<int>> dp(n, vector<int>(3, -1));
+        return callength(0, n, 0, nums, dp);
     }
 };
