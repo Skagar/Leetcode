0e61cd36aaf6d3e700a77467b5efea7056048c01
Samaksha Agarwal <gsamaksha@gmail.com>
2026-08-03 16:58:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (9.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
+        if (n == 0)
+            return;
+        int i = m - 1;
         int j = 0;
-        vector<int> temp;
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                temp.push_back(nums1[i]);
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                temp.push_back(nums2[j]);
+        while (i >= 0 && j < n) {
+            if (nums1[i] > nums2[j]) {
+                swap(nums1[i], nums2[j]);
+                i--;
                 j++;
-            } else {
-                temp.push_back(nums1[i]);
-                temp.push_back(nums1[i]);
-                i++;
+            } else
                 j++;
-            }
         }
-        while (i < m) {
-            temp.push_back(nums1[i]);
-            i++;
+        for (int k = m; k < m + n; k++) {
+            nums1[k] = nums2[k - m];
         }
-        while (j < n) {
-            temp.push_back(nums2[j]);
-            j++;
-        }
-        nums1.clear();
-        for (int k = 0; k < temp.size(); k++) {
-            nums1.push_back(temp[k]);
-        }
+       sort(nums1.begin(),nums1.end());
     }
 };
