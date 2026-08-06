5d16c426562d247839a0b9cb08b5d9ff739eb0aa
Samaksha Agarwal <gsamaksha@gmail.com>
2026-08-06 19:22:34 +0530

    Time: 53 ms (15.57%), Space: 32.5 MB (24.5%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        /* int m = intervals.size();
+         vector<int> ans(m);
+         for (int i = 0; i < m; i++) {
+             int minval = INT_MAX;
+             int ind = -1;
+             for (int j = 0; j < m; j++) {
+                 if (intervals[j][0] >= intervals[i][1]) {
+                     if (intervals[j][0] < minval) {
+                         minval = intervals[j][0];
+                         ind = j;
+                     }
+                 }
+             }
+             ans[i] = ind;
+         }
+         return ans;*/
         int m = intervals.size();
         vector<int> ans(m);
+        map<pair<int, int>, int> mp;
+        for (int i = 0; i < m; i++) {
+            int u = intervals[i][0];
+            int v = intervals[i][1];
+            mp[{u, v}] = i;
+        }
+        sort(intervals.begin(), intervals.end());
         for (int i = 0; i < m; i++) {
             int minval = INT_MAX;
             int ind = -1;
-            for (int j = 0; j < m; j++) {
-                if (intervals[j][0] >= intervals[i][1]) {
-                    if (intervals[j][0] < minval) {
-                        minval = intervals[j][0];
-                        ind = j;
+            int s = i;
+            int e = m - 1;
+            int u = intervals[i][0];
+            int v = intervals[i][1];
+            while (s <= e) {
+                int m = s + (e - s) / 2;
+                if (intervals[m][0] >= v) {
+                    if (intervals[m][0] < minval) {
+                        minval = intervals[m][0];
+                        ind = mp[{intervals[m][0], intervals[m][1]}];
                     }
-                }
+                    e = m - 1;
+                } else
+                    s = m + 1;
             }
-            ans[i] = ind;
+            ans[mp[{u, v}]] = ind;
         }
         return ans;
     }
