68f4c6477e1e2ae35d67bee1fdf5ed518b07c8df
Samaksha Agarwal <gsamaksha@gmail.com>
2026-08-12 19:11:23 +0530

    Time: 67 ms (46.74%), Space: 149.4 MB (56.74%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,38 +1,22 @@
 class Solution {
-    /*bool check(map<int, int>& mp, int& k) {
-        for (auto i : mp) {
-            if (i.second > k)
-                return false;
-        }
-        return true;
-    }*/
-
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (n == 1)
-            return 1;
-        map<int, int> mp;
-        int l = 0, r = 0;
-        int maxlen = INT_MIN;
-        int maxfq = INT_MIN;
-        while (l <= r && r < n) {
-            mp[nums[r]]++;
-            maxfq = max(maxfq, mp[nums[r]]);
-            if (maxfq <= k) {
-                maxlen = max(maxlen, r - l + 1);
-            } else {
-                while (l <= r && maxfq > k) {
-                    mp[nums[l]]--;
-                    if (mp[nums[l]] >= k)
-                        maxfq = min(maxfq, mp[nums[l]]);
-                    if (mp[nums[l]] == 0)
-                        mp.erase(nums[l]);
-                    l++;
-                }
+        unordered_map<int, int> freq;
+
+        int l = 0;
+        int ans = 0;
+
+        for (int r = 0; r < nums.size(); r++) {
+            freq[nums[r]]++;
+
+            while (freq[nums[r]] > k) {
+                freq[nums[l]]--;
+                l++;
             }
-            r++;
+
+            ans = max(ans, r - l + 1);
         }
-        return maxlen;
+
+        return ans;
     }
 };
