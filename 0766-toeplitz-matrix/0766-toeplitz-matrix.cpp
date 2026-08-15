a9b819e316e173e255f2e9b0e596e194e7c3360d
Samaksha Agarwal <gsamaksha@gmail.com>
2026-08-15 14:40:22 +0530

    Time: 11 ms (1.83%), Space: 24 MB (27.4%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,35 +1,20 @@
 class Solution {
-private:
-    bool dfs(int i, int j, int& ele, vector<vector<int>>& vis,
-             vector<vector<int>>& matrix) {
-        vis[i][j] = 1;
-        if (((i + 1) >= 0) && ((j + 1) >= 0) && ((i + 1) < matrix.size()) &&
-            ((j + 1) < matrix[0].size()) && (vis[i + 1][j + 1] == 0)) {
-            i = i + 1;
-            j = j + 1;
-            bool check = dfs(i, j, ele, vis, matrix);
-            if (check == false)
-                return false;
-        }
-        if (matrix[i][j] != ele)
-            return false;
-        else
-            return true;
-    }
-
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
+        map<int, vector<int>> mp;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (vis[i][j] == 0) {
-                    int ele = matrix[i][j];
-                    bool ans = dfs(i, j, ele, vis, matrix);
-                    if (ans == false)
-                        return false;
-                }
+                mp[i - j].push_back(matrix[i][j]);
+            }
+        }
+        for (auto& i : mp) {
+            vector<int> temp = i.second;
+            int ele = temp[0];
+            for (int j = 0; j < temp.size(); j++) {
+                if (temp[j] != ele)
+                    return false;
             }
         }
         return true;
