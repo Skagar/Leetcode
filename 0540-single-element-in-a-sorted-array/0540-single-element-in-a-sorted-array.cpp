5e908c765eecd593d1fb7b3fa08e6b3153d326a3
Samaksha Agarwal <gsamaksha@gmail.com>
2026-09-19 14:58:04 +0530

    Time: 0 ms (100%), Space: 26.4 MB (8.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,15 +4,16 @@ public:
         int n = nums.size();
         if (n == 1)
             return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
         int s = 0;
         int e = n - 1;
+
         while (s <= e) {
             int m = s + (e - s) / 2;
-            if (m == 0 && nums[m] != nums[m + 1])
-                return nums[m];
-            else if (m == n - 1 && nums[m] != nums[m - 1])
-                return nums[m];
-            else if (nums[m] != nums[m - 1] && nums[m] != nums[m + 1])
+            if (nums[m] != nums[m - 1] && nums[m] != nums[m + 1])
                 return nums[m];
             else if (nums[m] == nums[m + 1]) {
                 if (m % 2 == 0)
